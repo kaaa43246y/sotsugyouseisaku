25d32908fa3c3407e58144f95861d6857a8dd5ef
Naoko Yamaguchi <kamaki.naoko@hotmail.co.jp>
2020-08-20 14:54:34 +0900

    branch作成

--- app.py
+++ app.py
@@ -6,20 +6,20 @@ import sqlite3
 app  = Flask(__name__)
 
 # session利用時にはシークレットキーが必要
-app.secret_key = 'sunabacoyatsushiro'
+app.secret_key = 'kamaki0721'
 
-@app.route("/dbtest")
-def dbtest():
-    conn = sqlite3.connect("flasktest.db")
+@app.route("/index")
+def index():
+    conn = sqlite3.connect("hougen.db")
     c = conn.cursor()
     #課題１ スタッフの誰か一名 （誰でも可）情報を取得するSQL
-    c.execute('SELECT  name,age,address from staff where id=2')
-    staff_info = c.fetchone()
+    c.execute('SELECT word,mean from hougen where id=1')
+    hougen = c.fetchone()
     c.close()
-    print(staff_info)
+    print(hougen)
 
     #課題２ スタッフの誰か一名（誰でも可）情報を表示するHTMLを作成し表示
-    return render_template("dbtest.html",staff_info = staff_info)
+    return render_template("index.html",hougen = hougen)
 
 
 
