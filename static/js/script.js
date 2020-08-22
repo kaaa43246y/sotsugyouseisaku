46d5df4eb0be882b02ecd5ba7f21ca75e8b3b21f
Kazuhiro Yoshizumi <kaaazuuumin@gmail.com>
2020-08-22 18:29:41 +0900

    クリック時　画像表示

---
+++ static/js/script.js
@@ -0,0 +1,84 @@
+$(function(){
+
+  // 使用例をclick時にポップアップ画像表示
+  $("popup1").click(function(){
+    $("popup1_modal").fadeIn();
+  });
+
+  $("popup2").click(function(){
+    $("popup2_modal").fadeIn();
+  });
+
+  $("popup3").click(function(){
+    $("popup3_modal").fadeIn();
+  });
+
+  $("popup4").click(function(){
+    $("popup4_modal").fadeIn();
+  });
+
+  $("popup5").click(function(){
+    $("popup5_modal").fadeIn();
+  });
+
+  $("popup101").click(function(){
+    $("popup101_modal").fadeIn();
+  });
+
+  $("popup102").click(function(){
+    $("popup102_modal").fadeIn();
+  });
+
+  $("popup103").click(function(){
+    $("popup103_modal").fadeIn();
+  });
+
+  $("popup104").click(function(){
+    $("popup104_modal").fadeIn();
+  });
+
+  $("popup105").click(function(){
+    $("popup105_modal").fadeIn();
+  });
+
+  $("popup201").click(function(){
+    $("popup201_modal").fadeIn();
+  });
+
+  $("popup202").click(function(){
+    $("popup202_modal").fadeIn();
+  });
+
+  $("popup203").click(function(){
+    $("popup203_modal").fadeIn();
+  });
+
+  $("popup204").click(function(){
+    $("popup204_modal").fadeIn();
+  });
+
+  $("popup205").click(function(){
+    $("popup205_modal").fadeIn();
+  });
+
+  $("popup301").click(function(){
+    $("popup301_modal").fadeIn();
+  });
+
+  $("popup302").click(function(){
+    $("popup302_modal").fadeIn();
+  });
+
+  $("popup303").click(function(){
+    $("popup303_modal").fadeIn();
+  });
+
+  $("popup304").click(function(){
+    $("popup304_modal").fadeIn();
+  });
+
+  $("popup305").click(function(){
+    $("popup305_modal").fadeIn();
+  });
+
+});
