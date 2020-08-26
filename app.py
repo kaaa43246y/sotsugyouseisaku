fe61b5321ee843f12576033f736ee5e2457b46b3
KazuhiroYoshizumi <kaaazuuumin@gmail.com>
2020-08-26 17:50:36 +0900

    DBdata連携

--- app.py
+++ app.py
@@ -12,37 +12,78 @@ app.secret_key = 'kamaki0721'
 def index():
     conn = sqlite3.connect("hougen.db")
     c = conn.cursor()
-    c.execute('SELECT word,mean from hougen join category on hougen.category_id=category.id where hougen.category_id=1')
-    # hougen_1 = c.fetchone()
-
-    hougen_list_1 = c.fetchone()
+    c.execute('SELECT word from hougen join category on hougen.category_id=category.id where hougen.category_id=1')
+    hougen_list_1 =[]
+    for row in c.fetchall():
+        hougen_list_1.append({"word":row[0]})
     print(hougen_list_1)
+    # hougen_1 = c.fetchone()
+    # hougen_list_1 = c.fetchall()
     # for row in c.fetchone():
     #     hougen_list_1.append({row[0],row[1]})
 
-    c.execute('SELECT word,mean from hougen join category on hougen.category_id=category.id where hougen.category_id=2')
-    # hougen_2 = c.fetchone()
+    c.execute('SELECT mean from hougen join category on hougen.category_id=category.id where hougen.category_id=1')
+    mean_1 =[]
+    for row in c.fetchall():
+        mean_1.append({"mean":row[0]})
+    print(mean_1)
+    # mean_1 = c.fetchall()
 
-    hougen_list_2 = c.fetchone()
+    c.execute('SELECT word from hougen join category on hougen.category_id=category.id where hougen.category_id=2')
+    hougen_list_2 =[]
+    for row in c.fetchall():
+        hougen_list_2.append({"word":row[0]})
+    print(hougen_list_2)
+    # hougen_2 = c.fetchone()
+    c.execute('SELECT mean from hougen join category on hougen.category_id=category.id where hougen.category_id=2')
+    mean_2 =[]
+    for row in c.fetchall():
+        mean_2.append({"mean":row[0]})
+
+    # hougen_list_2 = c.fetchone()
     # for row in c.fetchall():
     #     hougen_list_2.append({row[0],row[1]})
 
-    c.execute('SELECT word,mean from hougen join category on hougen.category_id=category.id where hougen.category_id=3')
+    c.execute('SELECT word from hougen join category on hougen.category_id=category.id where hougen.category_id=3')
+    hougen_list_3 =[]
+    for row in c.fetchall():
+        hougen_list_3.append({"word":row[0]})
+    print(hougen_list_3)
     # hougen_3 = c.fetchone()
+    c.execute('SELECT mean from hougen join category on hougen.category_id=category.id where hougen.category_id=3')
+    mean_3 =[]
+    for row in c.fetchall():
+        mean_3.append({"mean":row[0]})
 
-    hougen_list_3 = c.fetchone()
+
+    # hougen_list_3 = c.fetchone()
     # for row in c.fetchall():
     #     hougen_list_3.append({row[0],row[1]})
 
-    c.execute('SELECT word,mean from hougen join category on hougen.category_id=category.id where hougen.category_id=4')
+    c.execute('SELECT word from hougen join category on hougen.category_id=category.id where hougen.category_id=4')
+    hougen_list_4 =[]
+    for row in c.fetchall():
+        hougen_list_4.append({"word":row[0]})
+    print(hougen_list_4)
     # hougen_4 = c.fetchone()
+    c.execute('SELECT mean from hougen join category on hougen.category_id=category.id where hougen.category_id=4')
+    mean_4 =[]
+    for row in c.fetchall():
+        mean_4.append({"mean":row[0]})
 
-    hougen_list_4 = c.fetchone()
+    # hougen_list_4 = c.fetchone()
     # for row in c.fetchall():
     #     hougen_list_4.append({row[0],row[1]})
     c.close()
     
-    return render_template("index.html",hougen_list_1=hougen_list_1[0],mean_1=hougen_list_1[1],hougen_list_2=hougen_list_2[0],mean_2=hougen_list_2[1],hougen_list_3=hougen_list_3[0],mean_3=hougen_list_3[1],hougen_list_4=hougen_list_4[0],mean_4=hougen_list_4[1])
+    return render_template("index.html",
+    hougen_list_1=hougen_list_1,mean_1=mean_1,
+    # hougen_list_1=hougen_list_1[1],mean_1=mean_1[1],
+
+    
+    hougen_list_2=hougen_list_2,mean_2=mean_2,
+    hougen_list_3=hougen_list_3,mean_3=mean_3,
+    hougen_list_4=hougen_list_4,mean_4=mean_4)
                                         
     
     # return render_template("index.html",word_1=hougen_1[0],
