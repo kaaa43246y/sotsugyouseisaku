150af72b06e1247f844467ba892b0801140a6c03
KazuhiroYoshizumi <kaaazuuumin@gmail.com>
2020-08-27 16:13:55 +0900

    隠しコマンド　開発者画面へ

--- static/js/script.js
+++ static/js/script.js
@@ -108,28 +108,55 @@ $(function(){
 
   });
   
-  // $("").click(function(){
-  //   $("#popup1_modal").fadeOut();
-  //   $("#popup2_modal").fadeOut();
-  //   $("#popup3_modal").fadeOut();
-  //   $("#popup4_modal").fadeOut();
-  //   $("#popup5_modal").fadeOut();
-  //   $("#popup101_modal").fadeOut();
-  //   $("#popup102_modal").fadeOut();
-  //   $("#popup103_modal").fadeOut();
-  //   $("#popup104_modal").fadeOut();
-  //   $("#popup105_modal").fadeOut();
-  //   $("#popup201_modal").fadeOut();
-  //   $("#popup202_modal").fadeOut();
-  //   $("#popup203_modal").fadeOut();
-  //   $("#popup204_modal").fadeOut();
-  //   $("#popup205_modal").fadeOut();
-  //   $("#popup301_modal").fadeOut();
-  //   $("#popup302_modal").fadeOut();
-  //   $("#popup303_modal").fadeOut();
-  //   $("#popup304_modal").fadeOut();
-  //   $("#popup305_modal").fadeOut();
-  // });
+  $(".black-background").click(function(){
+    $("#popup1_modal").fadeOut();
+    $("#popup2_modal").fadeOut();
+    $("#popup3_modal").fadeOut();
+    $("#popup4_modal").fadeOut();
+    $("#popup5_modal").fadeOut();
+    $("#popup101_modal").fadeOut();
+    $("#popup102_modal").fadeOut();
+    $("#popup103_modal").fadeOut();
+    $("#popup104_modal").fadeOut();
+    $("#popup105_modal").fadeOut();
+    $("#popup201_modal").fadeOut();
+    $("#popup202_modal").fadeOut();
+    $("#popup203_modal").fadeOut();
+    $("#popup204_modal").fadeOut();
+    $("#popup205_modal").fadeOut();
+    $("#popup301_modal").fadeOut();
+    $("#popup302_modal").fadeOut();
+    $("#popup303_modal").fadeOut();
+    $("#popup304_modal").fadeOut();
+    $("#popup305_modal").fadeOut();
+  });
 
+  
+  //入力されたキーを保存する
+  var inputKey = [];
+
+  //入力されたキーと比較する隠しコマンド
+  //38 = 上
+  //40 = 下
+  //37 = 左
+  //39 = 右
+  //66 = B
+  //65 = A
+  var konamiCommand = [38,38,40,40,37,39,37,39,66,65];
+
+ //画面上のキー入力イベントリスナ
+ $(window).keyup(function(e) {
+   //キー入力を配列に追加
+   inputKey.push(e.keyCode);
+
+   //キー入力が保存された配列と隠しコマンドを比較
+   if (inputKey.toString().indexOf(konamiCommand) >= 0) {
+
+      //隠しコマンド成功時の何らかの処理
+      window.open('/edit', '_blank')
+      //キー入力を初期化
+      inputKey = [];
+   }
+ });
 
 });
